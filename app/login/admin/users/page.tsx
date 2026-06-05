c94a51933b1604023031c13ee224eb18127fba31
Bradley Charles <bradgcharles@gmail.com>
2026-06-04 17:12:29 -0700

    fix: import React to avoid potential issues with JSX syntax in ManageUsersContent

--- app/login/admin/users/page.tsx
+++ app/login/admin/users/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect, useCallback, useMemo } from 'react'
+import React, { useState, useEffect, useCallback, useMemo } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import RouteGuard from '@/components/RouteGuard'
@@ -624,7 +624,7 @@ function ManageUsersContent() {
                                 </td>
                               </tr>
                             )}
-                          </>
+                          </React.Fragment>
                         )
                       })}
                     </tbody>
